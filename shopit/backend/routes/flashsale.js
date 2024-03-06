088e49baa65722b18e329ab7e8af3b29cfe44f13
WeePui <102616951+WeePui@users.noreply.github.com>
2024-03-06 15:40:02 +0700

    Delete shopit directory

--- shopit/backend/routes/flashsale.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { newFlashsale, getFlashes } = require('../controllers/flashsaleControner');
-const router = express.Router();
-
-
-router.route('/admin/newflashsale').post(newFlashsale);
-router.route('/flashsales').get(getFlashes);
-
-module.exports = router;
